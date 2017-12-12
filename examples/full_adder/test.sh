5b457db2bb7accd64a86d0daae8a969d037e4faf
1138-4EB <1138-4EB@users.noreply.github.com>
2017-12-12 04:58:22 +0100

    add first example: full_adder

---
+++ examples/full_adder/test.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+scriptdir="$(cd `dirname $0`; pwd)"
+
+ghdl -a "$scriptdir"/*.vhdl
+
+printf "[TEST] ghdl -r --vcd\n"
+ghdl -r adder_tb --vcd=adder.vcd
+
+printf "[TEST] ghdl -r --disp-tree=none\n"
+ghdl -r adder_tb --disp-tree=none > disp-tree_none.txt
+
+printf "[TEST] ghdl -r --disp-tree=inst\n"
+ghdl -r adder_tb --disp-tree=inst > disp-tree_inst.txt
+
+printf "[TEST] ghdl -r --disp-tree=proc\n"
+ghdl -r adder_tb --disp-tree=proc > disp-tree_proc.txt
+
+printf "[TEST] ghdl -r --disp-tree=port\n"
+ghdl -r adder_tb --disp-tree=port > disp-tree_port.txt
+
+printf "[TEST] ghdl -r --disp-tree\n"
+ghdl -r adder_tb --disp-tree > disp-tree.txt
+
+printf "[TEST] ghdl --pp-html\n"
+ghdl --pp-html "$scriptdir"/*.vhdl > adder.html
+
+printf "[TEST] ghdl --file-to-xml\n"
+ghdl --file-to-xml "$scriptdir"/*.vhdl > adder.xml
+
+#gtkwave adder.vcd
--- travis/travis.sh
+++ travis/travis.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
-mkdir release
-touch release/thi_is_a_release.tgz
-ls -la release
+mkdir -pv outputs
+
+echo "$0"
+echo "$(dirname $0)"
+echo "$(dirname $0)/.."
+echo "$(cd `dirname $0`/..; pwd)"
+
+for e in $(pwd)/examples/*; do
+  echo "$e"
+  p="outputs/$(basename $e)/ghdl"
+  mkdir -pv "$p"
+  v_src="/$(cd `dirname $0`/..; pwd)/examples/$(basename $e)://src"
+  v_wrk="/$(pwd)/$p://work"
+  echo "v_src $v_src"
+  echo "v_wrk $v_wrk"
+  docker run --rm -t \
+             -v "$v_src" \
+			 -v "$v_wrk" \
+			 -w "//work" \
+			 ghdl/ghdl:stretch-mcode-gpl \
+			 bash -c "//src/test.sh"
+done
+
+mkdir -pv release
+tar -zcvf "release/outputs.tgz" outputs
